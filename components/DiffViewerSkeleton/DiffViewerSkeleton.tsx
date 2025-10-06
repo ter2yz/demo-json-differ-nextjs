b616928291d9262d725ff226e80c98cea71efdb0
terryzzZ <zhengterry2012@gmail.com>
2025-10-06 12:23:14 +1100

    refactor: update diff viewer skeleton

--- components/DiffViewerSkeleton/DiffViewerSkeleton.tsx
+++ components/DiffViewerSkeleton/DiffViewerSkeleton.tsx
@@ -1,33 +1,38 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
 
 export default function DiffViewerSkeleton() {
+  const widths = [
+    "w-full",
+    "w-11/12",
+    "w-11/12",
+    "w-11/12",
+    "w-full",
+    "w-11/12",
+    "w-10/12",
+    "w-10/12",
+    "w-10/12",
+    "w-11/12",
+    "w-full",
+  ];
+
   return (
-    <div className="flex w-full gap-4">
-      <div className="flex w-full flex-col items-end gap-3">
-        <Skeleton className="h-3 w-full rounded" />
-        <Skeleton className="h-3 w-11/12 rounded" />
-        <Skeleton className="h-3 w-11/12 rounded" />
-        <Skeleton className="h-3 w-11/12 rounded" />
-        <Skeleton className="h-3 w-full rounded" />
-        <Skeleton className="h-3 w-11/12 rounded" />
-        <Skeleton className="h-3 w-10/12 rounded" />
-        <Skeleton className="h-3 w-10/12 rounded" />
-        <Skeleton className="h-3 w-10/12 rounded" />
-        <Skeleton className="h-3 w-11/12 rounded" />
-        <Skeleton className="h-3 w-full rounded" />
+    <div className="flex w-full gap-6 px-4">
+      <div className="flex w-full flex-col items-end gap-6 opacity-20">
+        {widths.map((width, index) => (
+          <Skeleton
+            key={index}
+            className={cn("h-3 rounded bg-rose-300", width)}
+          />
+        ))}
       </div>
-      <div className="flex w-full flex-col items-end gap-3">
-        <Skeleton className="h-3 w-full rounded" />
-        <Skeleton className="h-3 w-11/12 rounded" />
-        <Skeleton className="h-3 w-11/12 rounded" />
-        <Skeleton className="h-3 w-11/12 rounded" />
-        <Skeleton className="h-3 w-full rounded" />
-        <Skeleton className="h-3 w-11/12 rounded" />
-        <Skeleton className="h-3 w-10/12 rounded" />
-        <Skeleton className="h-3 w-10/12 rounded" />
-        <Skeleton className="h-3 w-10/12 rounded" />
-        <Skeleton className="h-3 w-11/12 rounded" />
-        <Skeleton className="h-3 w-full rounded" />
+      <div className="flex w-full flex-col items-end gap-6 opacity-20">
+        {widths.map((width, index) => (
+          <Skeleton
+            key={index}
+            className={cn("h-3 rounded bg-cyan-300", width)}
+          />
+        ))}
       </div>
     </div>
   );
